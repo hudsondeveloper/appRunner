0a67985001ef3576577d031b6579ebffe2b8308f
Hudson Luis <hudsonluis.costa@gmail.com>
2023-03-09 15:52:19 -0300

    fix apprunner.yaml

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-import express from "express";
-import fetch from "node-fetch";
-var app = express();
+const express = require("express");
+//const fetch = require("node-fetch");
+//import { nodeSDKBuilder } from "./nodeSDK";
+var app;
 
-app.listen(process.env.APP_PORT, () => {
-  console.log("rodou na porta", process.env.APP_PORT);
+app = express();
+//nodeSDKBuilder().then(() => {});
+
+app.listen(process.env.APP_PORT || 3001, () => {
+  console.log("rodou na porta", process.env.APP_PORT || 3001);
 });
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-app.get("/route", async function (req, res) {
-  const cep = await fetch("https://viacep.com.br/ws/41320010/json").then(
-    (data) => data.json()
-  );
-  res.send({ cep: cep });
-});
+// app.get("/route", async function (req, res) {
+//   const cep = await fetch("https://viacep.com.br/ws/41320010/json").then(
+//     (data) => data.json()
+//   );
+//   res.send({ cep: cep });
+// });
---
+++ nodeSDK.js
@@ -0,0 +1,78 @@
+const process = require("process");
+const opentelemetry = require("@opentelemetry/sdk-node");
+const { Resource, detectResources } = require("@opentelemetry/resources");
+
+const {
+  SemanticResourceAttributes,
+} = require("@opentelemetry/semantic-conventions");
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-grpc");
+const { AWSXRayPropagator } = require("@opentelemetry/propagator-aws-xray");
+const { AWSXRayIdGenerator } = require("@opentelemetry/id-generator-aws-xray");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { AwsInstrumentation } = require("opentelemetry-instrumentation-aws-sdk");
+const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
+const {
+  OTLPMetricExporter,
+} = require("@opentelemetry/exporter-metrics-otlp-grpc");
+const {
+  awsEcsDetector,
+  awsEc2Detector,
+} = require("@opentelemetry/resource-detector-aws");
+const { DiagConsoleLogger, DiagLogLevel, diag } = require("@opentelemetry/api");
+
+const _resource = Resource.default().merge(
+  new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "js-sample-app",
+  })
+);
+const _traceExporter = new OTLPTraceExporter();
+const _spanProcessor = new BatchSpanProcessor(_traceExporter);
+
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+const _tracerConfig = {
+  idGenerator: new AWSXRayIdGenerator(),
+};
+
+const _metricReader = new PeriodicExportingMetricReader({
+  exporter: new OTLPMetricExporter(),
+  exportIntervalMillis: 1000,
+});
+
+const nodeSDKBuilder = async () => {
+  const sdk = new opentelemetry.NodeSDK({
+    textMapPropagator: new AWSXRayPropagator(),
+    metricReader: _metricReader,
+    instrumentations: [
+      new HttpInstrumentation(),
+      new AwsInstrumentation({
+        suppressInternalInstrumentation: true,
+      }),
+    ],
+    resource: _resource,
+    spanProcessor: _spanProcessor,
+    traceExporter: _traceExporter,
+  });
+  sdk.configureTracerProvider(_tracerConfig, _spanProcessor);
+
+  const resource = await detectResources({
+    detectors: [awsEcsDetector, awsEc2Detector],
+  });
+
+  const tracerProvider = new NodeTracerProvider({ resource });
+  // this enables the API to record telemetry
+  await sdk.start();
+  // gracefully shut down the SDK on process exit
+  process.on("SIGTERM", () => {
+    sdk
+      .shutdown()
+      .then(() => console.log("Tracing and Metrics terminated"))
+      .catch((error) =>
+        console.log("Error terminating tracing and metrics", error)
+      )
+      .finally(() => process.exit(0));
+  });
+};
